ef664ca3126857218955472f03871c0ed2ed4a7d
freenjps <vladislavyanp@gmail.com>
2025-04-15 23:24:12 +0200

    Added a new Dice Game and Coin Inventory

--- Character Creation Project/obj/Debug/net9.0/Character Creation Project.AssemblyInfo.cs
+++ Character Creation Project/obj/Debug/net9.0/Character Creation Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Character Creation Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10ffc46ec683e253719698ee508801bc46d270c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fda53f272f1b1a59c1825a4bd6a5cc1d3e01f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Character Creation Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Character Creation Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
